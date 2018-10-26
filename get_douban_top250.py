b2e1c47717f661ddbaba0cdeb453c85d894404bb
eric.zhai <896895240@qq.com>
2018-10-26 10:03:35 +0800

    实现抓取豆瓣top250电影存入excel

--- get_douban_top250.py
+++ get_douban_top250.py
@@ -1,49 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import xlwt
 
-douban_url = "https://movie.douban.com/top250"
+douban_list = []
+result = []
 
+def get_douban_list():
+    for i in range(0, 250, 25):
+        douban_url = "https://movie.douban.com/top250?start={0}&filter=".format(i)
+        douban_list.append(douban_url)
+    return douban_list
 
+def getHtmlPage(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'}
+    response = requests.get(url, headers=headers)
+    htmlPage = response.text
+    return htmlPage
 
-class DSpider(object):
+def getInfoFromOnePage(htmlPage):
+    soup = BeautifulSoup(htmlPage, 'html.parser')
+    movies = soup.find('ol', class_='grid_view').find_all('li')
+    for i in range(0, 25):
+        movieName = movies[i].find('span').string
+        movieScore = movies[i].find('span', class_='rating_num').string
+        movieNum = movies[i].find('div', class_='star').find_all('span')[-1].string.strip('人评价')
+        movieQuote = movies[i].find('p', class_='quote').find('span').string
+        result.append([movieName, movieScore, movieNum, movieQuote])
+    return result
 
-    def getHtmlPage(self, url):
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'}
-        response = requests.get(url, headers=headers)
-        htmlPage = response.text
-        return htmlPage
+if '__main__':
+    book = xlwt.Workbook()
+    sheet = book.add_sheet('movie_sheet')
 
-    def getInfoFromOnePage(self,url):
-        dog = self.getHtmlPage(douban_url)
-        soup = BeautifulSoup(dog, 'html.parser')
-        result = []
-        movies = self.soup.find('ol', class_='grid_view').find_all('li')
-        for i in range(0, 25):
-            movieName = movies[i].find('span').string
-            movieScore = movies[i].find('span', class_='rating_num').string
-            movieNum = movies[i].find('div', class_='star').find_all('span')[-1].string.strip('人评价')
-            movieQuote = movies[i].find('p', class_='quote').find('span').string
-            result.append([i + 1, movieName, movieScore, movieNum, movieQuote])
-        return result
+    for i in get_douban_list():
+        htmlPage = getHtmlPage(i)
+        result = getInfoFromOnePage(htmlPage)
+    result.insert(0, ['电影名', '评分', '评价人数', '短评'])
 
-    def get_other_url(self):
-        dog = self.getHtmlPage(douban_url)
-        soup = BeautifulSoup(dog, 'html.parser')
-        for i in range(2, 10):
-            page = soup.find('div', class_='paginator').find_all('a', href=True)[:-1]
-        for i in page:
-            newUrl = douban_url + i['href']
-            print(newUrl)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for row in range(0, 251):
+        for col in range(0, 4):
+            sheet.write(row, col, result[row][col])
+    book.save('test.xls')
