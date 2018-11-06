a81579b09dc38976b7109ce2f08d7050bba671e4
eric.zhai <896895240@qq.com>
2018-11-06 14:50:11 +0800

    增加读取文件的操作

---
+++ LXF_Python/IO_test/with_file_write.py
@@ -0,0 +1,10 @@
+
+#你回发现原来的文件内容被覆盖了
+# f = open('E:\\Python\\LXF_Python\\IO_test\\test.txt','w')
+# f.write("HELLO ZHAI")
+# f.close()
+
+#模式选用a+，在文件后追加
+# f = open('E:\\Python\\LXF_Python\\IO_test\\test.txt','a+')
+# f.write("HELLO ZHAI")
+# f.close()
