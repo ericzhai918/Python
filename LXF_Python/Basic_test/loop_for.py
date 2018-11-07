9da8bb966880b034c306a0f0d61558e10241408b
zll <896895240@qq.com>
2018-11-08 06:19:25 +0800

    .

--- LXF_Python/Basic_test/loop_for.py
+++ LXF_Python/Basic_test/loop_for.py
@@ -1,14 +1,15 @@
-# print(1+2+3+4+5)
+print(1+2+3+4+5)
 
-# names = ['Lisa','Alex','Lucy']
-# for name in names:
-#     print(name)
-
+names = ['Lisa','Alex','Lucy']
+for name in names:
+    print(name)
+#传统
 sum = 0
 for x in [1,2,3,4,5,6,7,8,9,10]:
     sum = sum + x
 print(sum)
 
+#使用range()
 sum = 0
 for x in range(0,11):
     sum = sum + x
