6a7274df492f2574369c2ace4b38c5b160708c61
eric.zhai <896895240@qq.com>
2018-11-07 10:44:05 +0800

    ..

--- LXF_Python/IO_test/with_file_write.py
+++ LXF_Python/IO_test/with_file_write.py
@@ -1,10 +1,10 @@
 
 #你回发现原来的文件内容被覆盖了
-# f = open('E:\\Python\\LXF_Python\\FIle_test\\test.txt','w')
-# f.write("HELLO ZHAI")
-# f.close()
+f = open('E:\\Python\\LXF_Python\\IO_test\\test.txt','w')
+f.write("HELLO ZHAI")
+f.close()
 
 #模式选用a+，在文件后追加
-# f = open('E:\\Python\\LXF_Python\\FIle_test\\test.txt','a+')
-# f.write("HELLO ZHAI")
-# f.close()
+f = open('E:\\Python\\LXF_Python\\IO_test\\test.txt','a+')
+f.write("HELLO ZHAI")
+f.close()
