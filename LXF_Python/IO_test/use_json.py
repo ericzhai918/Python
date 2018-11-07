d5abb867bbd3ec90f5280545f9257317574915d3
eric.zhai <896895240@qq.com>
2018-11-07 15:05:44 +0800

    .

--- LXF_Python/IO_test/use_json.py
+++ LXF_Python/IO_test/use_json.py
@@ -1,14 +1,14 @@
 import json
 
-#Python对象到JSON格式的转换
-d = dict(name='Bob',age=20,score=88)
-json_str= json.dumps(d)
-print("Python内置的数据类型为:",d)
+# Python对象到JSON格式的转换
+d = dict(name='Bob', age=20, score=88)
+json_str = json.dumps(d)
+print("Python内置的数据类型为:", d)
 print(type(d))
-print('经Json格式转换后:',b)
+print('经Json格式转换后:', b)
 print(type(json_str))
 
-#JSON格式转换Python对象
+# JSON格式转换Python对象
 json_str = '{"age":20,"score":88,"name":"bob"}'
 d = json.loads(json_str)
 print(d)
