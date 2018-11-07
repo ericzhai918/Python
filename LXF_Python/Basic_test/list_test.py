96309b65d4118f4d8bfbf50ff8d221346e2bb98e
zll <896895240@qq.com>
2018-11-08 06:48:56 +0800

    list

---
+++ LXF_Python/Basic_test/list_test.py
@@ -0,0 +1,24 @@
+classmates = ['Lisa','Lucy','Alex']
+print(classmates)
+print(len(classmates))
+print(classmates[0])
+print(classmates[1])
+print(classmates[2])
+print(classmates[-1])
+print(classmates[-2])
+classmates.append('Judy')
+print(classmates)
+classmates.insert(0,'Bae')
+print(classmates)
+classmates.pop()
+print(classmates)
+classmates.pop(1)
+print(classmates)
+classmates[0]='Tina'
+print(classmates)
+
+list=[]
+print(list)
+
+list1=[True,111,'A']
+print(list1)
