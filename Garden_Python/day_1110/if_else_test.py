bec32e62fcbea67b35a754c6c42589f01a1cf427
eric.zhai <896895240@qq.com>
2018-11-10 17:24:14 +0800

    if..else

---
+++ Garden_Python/day_1110/if_else_test.py
@@ -0,0 +1,34 @@
+# if
+# age = 20
+# if age >= 18:
+#     print('your age is:',age)
+#     print('Adult')
+
+#让年龄变为可选
+# age = int(input("Please input your age:"))
+# if age >= 18:
+#     print("you age is:",age)
+#     print('Adult')
+
+# if...else
+# age = 3
+# if age >= 18:
+#     print('your age is:',age)
+#     print('adult')
+# else:
+#     print('your age is:',age)
+#     print('teenager')
+
+#if..elif..else
+# age = 20
+# if age >= 6:
+#     print('teenager')
+# elif age > 18:
+#     print('adult')
+# else:
+#     print('kid')
+
+#简写
+# x = 'S'#非空
+# if x:
+#     print(True)
