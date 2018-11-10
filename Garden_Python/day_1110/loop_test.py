54d4a8c3a5dd872003cec322c32c34f14d60f592
eric.zhai <896895240@qq.com>
2018-11-10 17:23:57 +0800

    for while循环

---
+++ Garden_Python/day_1110/loop_test.py
@@ -0,0 +1,25 @@
+
+#for..in循环list
+# names = ['A','B','C']
+# for name in names:
+#     print(name)
+
+#for..in累加1-10
+# sum = 0
+# for x in [1,2,3,4,5,6,7,8,9,10]:
+#     sum = sum + x
+# print(sum)
+
+#for...in累加1-1000
+# sum = 0
+# for x in range(1001):
+#     sum = sum + x
+# print(sum)
+
+#while计算100内所有奇数的和
+# sum = 0
+# n = 99
+# while n > 0:
+#     sum = sum + n
+#     n = n - 2
+# print(sum)
