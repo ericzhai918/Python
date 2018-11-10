5942e1c9362373f624a18b261531a5b002317e09
eric.zhai <896895240@qq.com>
2018-11-10 17:22:45 +0800

    学生成绩等级分类练习

---
+++ Garden_Python/day_1110/grade_printer.py
@@ -0,0 +1,33 @@
+# grade = 55
+# if grade >= 90:
+#     print("Your grade is:", grade)
+#     print("Level A")
+# elif grade >= 80:
+#     print("Your grade is:", grade)
+#     print("Level B")
+# elif grade >= 70:
+#     print("Your grade is:", grade)
+#     print("Level C")
+# elif grade >= 60:
+#     print("Your grade is:", grade)
+#     print("Level D")
+# else:
+#     print('God will help you!')
+
+def grade_printer(grade):
+    if grade >= 90:
+        print("Your grade is:", grade)
+        print("Level A")
+    elif grade >= 80:
+        print("Your grade is:", grade)
+        print("Level B")
+    elif grade >= 70:
+        print("Your grade is:", grade)
+        print("Level C")
+    elif grade >= 60:
+        print("Your grade is:", grade)
+        print("Level D")
+    else:
+        print('God will help you!')
+
+grade_printer(65)
