5aca4ff8c2957faad9b0ea9fe9882f39cd64b358
eric.zhai <896895240@qq.com>
2018-11-14 17:21:22 +0800

    序列去重

---
+++ My_Python_Test/ele_distinct_keep_order.py
@@ -0,0 +1,27 @@
+#去重并保持顺序
+
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+
+a = [1, 5, 2, 1, 9, 1, 5, 10]
+print(list(dedupe(a)))
+
+#去重并保持顺序（不可哈希的元素）
+
+def dedupe(items,key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+a = [ {'x':1, 'y':2}, {'x':1, 'y':3}, {'x':1, 'y':2}, {'x':2, 'y':4}]
+c= dedupe(a, key=lambda d: (d['x'],d['y']))
+b = list(c)
+print(b)
