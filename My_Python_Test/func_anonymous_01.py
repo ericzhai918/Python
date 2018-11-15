dff598aa4a427fb64cbb439dd40819cea5557197
eric.zhai <896895240@qq.com>
2018-11-15 19:05:58 +0800

    匿名函数

---
+++ My_Python_Test/func_anonymous_01.py
@@ -0,0 +1,3 @@
+fruits = ['strawberry', 'fig', 'apple', 'cherry', 'raspberry', 'banana']
+a = sorted(fruits, key=lambda word: word[::-1])
+print(a)
---
+++ My_Python_Test/func_anonymous_02.py
@@ -0,0 +1,3 @@
+sum = lambda a, b: a + b
+print(sum(10, 20))
+print(sum(1, 2))
