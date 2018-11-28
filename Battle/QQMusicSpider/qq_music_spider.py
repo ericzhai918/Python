558e2d22a979acbdc701d934d03e2b5ad591158f
zll <896895240@qq.com>
2018-11-28 16:25:31 +0800

    增加下载函数saveMusic

--- Battle/QQMusicSpider/qq_music_spider.py
+++ Battle/QQMusicSpider/qq_music_spider.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 import time
 import math
 from qq_music_spider_db import *
+import os
+import re
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.146 Safari/537.36'
@@ -11,7 +13,7 @@ headers = {
 
 session = requests.session()
 
-def download(guid, songmid,cookie_dict):
+def download(guid, songmid,cookie_dict,songname,albumname):
     url = 'https://u.y.qq.com/cgi-bin/musicu.fcg?loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0'\
           '&platform=yqq&needNewCode=0&data=%7B%22req%22%3A%7B%22module%22%3A%22CDN.SrfCdnDispatchServer%22%2C%22method%22%'\
           '3A%22GetCdnDispatch%22%2C%22param%22%3A%7B%22guid%22%3A%22'+ guid + '%22%2C%22calltype%22%3A0%2C%22userip%22%3A%22'\
@@ -25,12 +27,34 @@ def download(guid, songmid,cookie_dict):
         url = "http://dl.stream.qqmusic.qq.com/%s" % (purl)
         print(url)
         r = requests.get(url, headers=headers)
-        f = open('E:\\Pythontest1\\song\\' + songmid + '.m4a', 'wb')
-        f.write(r.content)
-        f.close()
+        realMusic = r.content
+        time.sleep(5)
+        saveMusic(songname,albumname,realMusic)
         return True
     else:
         return False
+
+def saveMusic(songname,albumname,realMusic):
+    folder = 'E:\\PythonTest\\DownloadSong'
+    albumname = re.sub(r'\W+', '_', albumname)
+    songname = re.sub(r'\W+', '_', songname)
+
+    albumPath = 'E:\\PythonTest\\DownloadSong\\' + albumname
+    song = albumPath + "\\"  + songname + '.m4a'
+    print(folder)
+    print(albumPath)
+    print(song)
+    if os.path.exists(folder) == False:
+        os.mkdir(folder)
+
+    if os.path.exists(albumPath) == False:
+        os.mkdir(albumPath)
+
+    if os.path.exists(song) == False:
+        f = open(albumPath + "\\" + songname + '.m4a', 'wb')
+        f.write(realMusic)
+        f.close()
+
 
 def getCookies():
     url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg?loginUin=0&hostUin=0&format=jsonp&inCharset=' \
@@ -68,11 +92,14 @@ def get_singer_songs(singermid,cookie_dict):
         for i in music_data:
             song_dict['song_name'] = i['musicData']['songname']
             song_dict['song_album'] = i['musicData']['albumname']
-            song_dict['song_interval'] = i['musicData']['interval']
+            song_dict['song_albummid'] = i['musicData']['albummid']
+            # song_dict['song_interval'] = i['musicData']['interval']
+            song_dict['song_interval']=str(i['musicData']['interval']//60)+'m'+str(i['musicData']['interval']%60)+'s'
             song_dict['song_songmid'] = i['musicData']['songmid']
             song_dict['song_singer'] = song_singer
+            song_dict['song_singermid'] = i['musicData']['singer'][0]['mid']
 
-            info = download(cookie_dict['pgv_pvid'],song_dict['song_songmid'],cookie_dict)
+            info = download(cookie_dict['pgv_pvid'],song_dict['song_songmid'],cookie_dict,song_dict['song_name'],song_dict['song_album'])
 
             if info:
                 insert_data(song_dict)
@@ -106,5 +133,7 @@ def get_all_singer():
 
 
 if __name__ == '__main__':
-    get_all_singer()
+    # get_all_singer()
+    cookies = getCookies()
+    get_singer_songs('001fNHEf1SFEFN',cookies)
 
