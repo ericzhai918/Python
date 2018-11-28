44be72ff4dc2e0100ff29aa4b16177100a92837d
zll <896895240@qq.com>
2018-11-28 13:23:20 +0800

    数据库文件

---
+++ Battle/QQMusicSpider/qq_music_spider_db.py
@@ -0,0 +1,42 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+#初始化数据库连接
+engine = create_engine("mysql+pymysql://root:123456@localhost:3306/qq_music_db?charset=utf8", echo=False)
+#创建会话对象，用于数据库的操作
+DBSession = sessionmaker(bind=engine)
+SQLsession = DBSession()
+#创建对象的基类
+Base = declarative_base()
+
+#映射数据表
+class Song(Base):
+    #表名
+    __tablename__ = 'song'
+    #字段，属性
+    song_id = Column(Integer,primary_key=True)
+    song_name = Column(String(100))
+    song_album = Column(String(100))
+    song_interval = Column(String(100))
+    song_songmid = Column(String(100))
+    song_singer = Column(String(100))
+#创建数据表
+Base.metadata.create_all(engine)
+#数据入库
+def insert_data(song_dict):
+    engine = create_engine('mysql+pymysql://root:123456@localhost:3306/qq_music_db?charset=utf8', echo=False)
+    DBSession = sessionmaker(bind=engine)
+    SQLsession = DBSession()
+    #创建新Song对象
+    data = Song(
+        song_name=song_dict['song_name'],
+        song_album=song_dict['song_album'],
+        song_interval=song_dict['song_interval'],
+        song_songmid=song_dict['song_songmid'],
+        song_singer=song_dict['song_singer'],
+    )
+    # 添加到session
+    SQLsession.add(data)
+    #提交即保存到数据库
+    SQLsession.commit()
