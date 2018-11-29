dad7b7e352804e43efdff440b35e31d767ebc93e
zll <896895240@qq.com>
2018-11-29 16:34:43 +0800

    读入与输出

---
+++ Pandas/Pandas_sao/Data_In_Out.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+########################数据输入输出########################
+#读取csv文件
+# pd.csv('input_file_name.csv')
+#创建csv文件
+# df.to_csv('output_file_name.csv')
+
+#读取和创建 excel 文件
+# pd.read_excel('input_excel_name.xlsx')
+# df.to_excel('output_excel_name.xlsx')
