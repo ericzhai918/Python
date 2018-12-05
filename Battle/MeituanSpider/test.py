e494b36f1c651dc01d197f0a7066cd88ab505cca
eric.zhai <896895240@qq.com>
2018-12-05 17:53:19 +0800

    redis的使用

--- Battle/MeituanSpider/test.py
+++ Battle/MeituanSpider/test.py
@@ -36,3 +36,15 @@
 # st = time.localtime(1543901662.429062)
 # a = time.strftime('%Y-%m-%d %H:%M:%S',st)
 # print(a)
+
+# 测试连接redis
+import redis
+
+r = redis.Redis(host='127.0.0.1', port=6379)
+r.set('1111', 'aaaaa')
+r.set('2222', 'bbbb')
+r.set('3333', 'cccc')
+print(r.get('1111'))
+
+r.setnx('1111','abc')
+print(r.get('1111'))
