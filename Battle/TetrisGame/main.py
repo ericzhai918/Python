d9dea29b4ebea7380edc2823ad34f8ac5d4c750c
eric.zhai <896895240@qq.com>
2018-12-07 14:45:04 +0800

    画一条直线

--- Battle/TetrisGame/main.py
+++ Battle/TetrisGame/main.py
@@ -2,12 +2,22 @@ import pygame
 from pygame.locals import *
 import sys
 
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 900
+CELL_WIDTH = 40
+GAME_AREA_WIDTH = CELL_WIDTH * 10
+GAME_AREA_HEIGHT = CELL_WIDTH * 20
+GAME_AREA_LEFT = (SCREEN_WIDTH - GAME_AREA_WIDTH) // 2
+GAME_AREA_TOP = SCREEN_HEIGHT - GAME_AREA_HEIGHT
+EDGE_COLOR = (0, 0, 0)
+CELL_COLOR = (100, 100, 100)
+BG_COLOR = (230, 230, 230)
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1000, 600))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption('俄罗斯方块')
-    bg_color = (230, 230, 230)
+    bg_color = BG_COLOR
 
     # 窗口主循环
     while True:
@@ -17,8 +27,14 @@ def main():
                 sys.exit()
 
         screen.fill(bg_color)
+        draw_game_area(screen)
         # 重绘界面
         pygame.display.flip()
+
+
+def draw_game_area(screen):
+    pygame.draw.line(screen, (0, 0, 0), (100, 100), (200, 200))
+    pass
 
 
 if __name__ == "__main__":
