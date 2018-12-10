3d956f3d963ead5fcd9ae07b01fcaa68d960783a
eric.zhai <896895240@qq.com>
2018-12-10 15:31:01 +0800

    声明方块类型

--- Battle/TetrisGame/settings.py
+++ Battle/TetrisGame/settings.py
@@ -1,4 +1,3 @@
-
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 900
 CELL_WIDTH = 40
@@ -9,3 +8,41 @@ GAME_AREA_TOP = SCREEN_HEIGHT - GAME_AREA_HEIGHT
 EDGE_COLOR = (0, 0, 0)
 CELL_COLOR = (100, 100, 100)
 BG_COLOR = (230, 230, 230)
+
+S_SHAPE_TEMPLATE = ['.OO.',
+                    'OO..',
+                    '....']
+
+Z_SHAPE_TEMPLATE = ['OO..',
+                    '.OO.',
+                    '....']
+
+I_SHAPE_TEMPLATE = ['.O..',
+                    '.O..',
+                    '.O..',
+                    '.O..']
+
+O_SHAPE_TEMPLATE = ['OO..',
+                    'OO..',
+                    '....']
+
+J_SHAPE_TEMPLATE = ['..O.',
+                    '..O.',
+                    '.OO.']
+
+L_SHAPE_TEMPLATE = ['.O..',
+                    '.O..',
+                    '.OO.']
+
+T_SHAPE_TEMPLATE = ['.O..',
+                    'OOO.',
+                    '....']
+
+PIECES = {
+    'S': S_SHAPE_TEMPLATE,
+    'Z': Z_SHAPE_TEMPLATE,
+    'J': J_SHAPE_TEMPLATE,
+    'L': L_SHAPE_TEMPLATE,
+    'O': O_SHAPE_TEMPLATE,
+    'T': T_SHAPE_TEMPLATE,
+}
