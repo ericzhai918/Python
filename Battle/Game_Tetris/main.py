7c4c5855f4f76703a23f3e84abfb626069c6aed5
eric.zhai <896895240@qq.com>
2018-12-24 15:36:46 +0800

    绘制小方块

--- Battle/Game_Tetris/main.py
+++ Battle/Game_Tetris/main.py
@@ -30,19 +30,19 @@ def main():
 
         screen.fill(bg_color)
         draw_game_area(screen)
+        draw_cell(screen, GAME_AREA_LR, GAMW_AREA_TOP)
 
         pygame.display.flip()
 
 
 def draw_game_area(screen):
     # 左
-    pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR, GAMW_AREA_TOP), (GAME_AREA_LR, SCREEN_HEIGHT))
-
+    # pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR, GAMW_AREA_TOP), (GAME_AREA_LR, SCREEN_HEIGHT))
     # 上
     # pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR, GAMW_AREA_TOP), (GAME_AREA_LR + GAME_AREA_WIDTH, GAMW_AREA_TOP))
     # 右
-    pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR + GAME_AREA_WIDTH, GAMW_AREA_TOP),
-                     (GAME_AREA_LR + GAME_AREA_WIDTH, SCREEN_HEIGHT))
+    # pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR + GAME_AREA_WIDTH, GAMW_AREA_TOP),
+    #                  (GAME_AREA_LR + GAME_AREA_WIDTH, SCREEN_HEIGHT))
     # 下
     # pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR, SCREEN_HEIGHT), (GAME_AREA_LR + GAME_AREA_WIDTH, SCREEN_HEIGHT))
 
@@ -55,6 +55,13 @@ def draw_game_area(screen):
     for n in range(0, 11):
         pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR + CELL_WIDTH * n, GAMW_AREA_TOP),
                          (GAME_AREA_LR + CELL_WIDTH * n, SCREEN_HEIGHT))
+
+
+def draw_cell(screen, left, top):
+    cell_left_top = (left, top)
+    cell_width_height = (CELL_WIDTH, CELL_WIDTH)
+    cell_rect = pygame.Rect(cell_left_top, cell_width_height)
+    pygame.draw.rect(screen, CELL_COLOR, cell_rect)
 
 
 if __name__ == '__main__':
