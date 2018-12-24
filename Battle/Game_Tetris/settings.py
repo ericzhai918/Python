d13e9c50f4b719c10e2c74c0d84b75a3d6d528bb
eric.zhai <896895240@qq.com>
2018-12-24 16:33:27 +0800

    增加方块的形状矩阵

--- Battle/Game_Tetris/settings.py
+++ Battle/Game_Tetris/settings.py
@@ -11,3 +11,57 @@ GAMW_AREA_TOP = SCREEN_HEIGHT - GAMW_AREA_HEIGHT
 BG_COLOR = (230, 230, 230)
 EDGE_COLOR = (0, 0, 0)
 CELL_COLOR = (100, 100, 100)
+
+S_SHAPE_TEMPLATE = [
+    '.OO.',
+    'OO..',
+    '....'
+]
+
+Z_SHAPE_TEMPLATE = [
+    'OO..',
+    '.OO.',
+    '....'
+]
+
+I_SHAPE_TEMPLATE = [
+    '.O..',
+    '.O..',
+    '.O..',
+    '.O..'
+]
+
+O_SHAPE_TEMPLATE = [
+    'OO',
+    'OO'
+]
+
+J_SHAPE_TEMPLATE = [
+    '..O.',
+    '..O.',
+    '.OO.'
+]
+
+L_SHAPE_TEMPLATE = [
+    '.O..',
+    '.O..',
+    '.OO.'
+]
+
+T_SHAPE_TEMPLATE = [
+    '.O..',
+    'OOO.',
+    '....'
+]
+
+PIECES = {
+    'S': S_SHAPE_TEMPLATE,
+    'Z': Z_SHAPE_TEMPLATE,
+    'J': J_SHAPE_TEMPLATE,
+    'L': L_SHAPE_TEMPLATE,
+    'I': I_SHAPE_TEMPLATE,
+    'O': O_SHAPE_TEMPLATE,
+    'T': T_SHAPE_TEMPLATE,
+}
+
+
