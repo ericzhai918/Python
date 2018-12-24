6b636aec5f32929508664e368e5219b411dfa835
eric.zhai <896895240@qq.com>
2018-12-24 14:58:31 +0800

    声明常量

--- Battle/Game_Tetris/main.py
+++ Battle/Game_Tetris/main.py
@@ -1,13 +1,27 @@
 import pygame
 import sys
 
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 900
+
+CELL_WIDTH = 40
+GAME_AREA_WIDTH = CELL_WIDTH * 10
+GAMW_AREA_HEIGHT = CELL_WIDTH * 20
+
+GAME_AREA_LR = (SCREEN_WIDTH - GAME_AREA_WIDTH) // 2
+GAMW_AREA_TOP = SCREEN_HEIGHT - GAMW_AREA_HEIGHT
+
+BG_COLOR = (230, 230, 230)
+EDGE_COLOR = (0, 0, 0)
+CELL_COLOR = (100, 100, 100)
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 900))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("俄罗斯方块")
 
-    bg_color = (230, 230, 230)
+    bg_color = BG_COLOR
 
     while True:
         for event in pygame.event.get():
