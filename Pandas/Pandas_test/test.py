b6de702f0e17f9fe02693ae9352ed7056540a292
eric.zhai <896895240@qq.com>
2018-12-24 11:37:57 +0800

    .

--- Pandas/Pandas_test/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df1 = pd.read_csv('1.txt',sep=' ',header='infer', encoding='GBK',error_bad_lines=False)
-df3 = pd.read_csv('3.txt',sep='\t',header='infer', encoding='GBK',error_bad_lines=False)
-df2 = pd.read_csv('2.txt',sep=' ',header='infer', encoding='GBK',error_bad_lines=False)
-
-
-print(pd.merge(df2, df1, on='虚机IP'))
-
