8515dc0618e380ec00f211d2c66c927c6e2121dd
eric.zhai <896895240@qq.com>
2018-12-25 10:13:36 +0800

    实现移动方块

--- Battle/Game_Tetris/main.py
+++ Battle/Game_Tetris/main.py
@@ -13,7 +13,7 @@ def main():
     piece = Piece('S', screen)
 
     while True:
-        check_events()
+        check_events(piece)
 
         screen.fill(bg_color)
         draw_game_area(screen)
@@ -22,19 +22,19 @@ def main():
         pygame.display.flip()
 
 
-def check_events():
+def check_events(piece):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                print("向下方向键被按下")
+                piece.move_down()
             elif event.key == pygame.K_UP:
                 print("向上方向键被按下")
             elif event.key == pygame.K_LEFT:
-                print("向左方向键被按下")
+                piece.move_left()
             elif event.key == pygame.K_RIGHT:
-                print("向右方向键被按下")
+                piece.move_right()
 
 
 def draw_game_area(screen):
