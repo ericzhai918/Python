c7a0f7e86288c423451dc2f6ae817bdabccae25f
eric.zhai <896895240@qq.com>
2018-12-28 11:40:53 +0800

    重构：生成网格函数draw_grid,增加函数：加载暂停游戏加载图片

--- Battle/Game_Tetris/gamedisplay.py
+++ Battle/Game_Tetris/gamedisplay.py
@@ -4,20 +4,24 @@ import pygame
 
 class GameDisplay():
     @staticmethod
-    def draw_game_area(screen, game_state):
+    def draw_game_area(screen, game_state,game_resource):
         '''绘制游戏区域'''
-        # 行，20行，纵坐标衡量增大
-        for row in range(0, 21):
-            pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR, GAMW_AREA_TOP + CELL_WIDTH * row),
-                             (GAME_AREA_LR + GAME_AREA_WIDTH, GAMW_AREA_TOP + CELL_WIDTH * row))
-
-        # 列，10列，横坐标衡量增大
-        for column in range(0, 11):
-            pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR + CELL_WIDTH * column, GAMW_AREA_TOP),
-                             (GAME_AREA_LR + CELL_WIDTH * column, SCREEN_HEIGHT))
-
+        GameDisplay.draw_grid(screen)
         GameDisplay.draw_wall(game_state.wall)
         GameDisplay.draw_score(screen, game_state.game_score)
+        if game_state.stopped:
+            GameDisplay.draw_start_prompt(screen, game_resource)
+
+    @staticmethod
+    def draw_grid(screen):
+        # 行，20行，纵坐标衡量增大
+        for row in range(0, 21):
+            pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR, GAME_AREA_TOP + CELL_WIDTH * row),
+                             (GAME_AREA_LR + GAME_AREA_WIDTH, GAME_AREA_TOP + CELL_WIDTH * row))
+        # 列，10列，横坐标衡量增大
+        for column in range(0, 11):
+            pygame.draw.line(screen, EDGE_COLOR, (GAME_AREA_LR + CELL_WIDTH * column, GAME_AREA_TOP),
+                             (GAME_AREA_LR + CELL_WIDTH * column, SCREEN_HEIGHT))
 
     @staticmethod
     def draw_wall(game_wall):
@@ -30,7 +34,7 @@ class GameDisplay():
     @staticmethod
     def draw_cell(screen, row, column, color):
         '''第row行column列的格子里填充color颜色。一种方块对应一种颜色。'''
-        cell_position = (column * CELL_WIDTH + GAME_AREA_LR + 1, row * CELL_WIDTH + GAMW_AREA_TOP + 1)
+        cell_position = (column * CELL_WIDTH + GAME_AREA_LR + 1, row * CELL_WIDTH + GAME_AREA_TOP + 1)
         cell_width_height = (CELL_WIDTH - 2, CELL_WIDTH - 2)
         cell_rect = pygame.Rect(cell_position, cell_width_height)
         pygame.draw.rect(screen, color, cell_rect)
@@ -40,7 +44,7 @@ class GameDisplay():
         '''绘制游戏得分'''
         score_label_font = pygame.font.SysFont('simhei', 28)
         score_label_surface = score_label_font.render(u'得分：', False, SCORE_LABEL_COLOR)
-        score_label_position = (GAME_AREA_LR + GAME_AREA_WIDTH + 40, GAMW_AREA_TOP)
+        score_label_position = (GAME_AREA_LR + GAME_AREA_WIDTH + 40, GAME_AREA_TOP)
         screen.blit(score_label_surface, score_label_position)
 
         score_font = pygame.font.SysFont('arial', 36)
@@ -48,3 +52,8 @@ class GameDisplay():
         score_label_width = score_label_surface.get_width()
         score_position = (score_label_position[0] + score_label_width, score_label_position[1])
         screen.blit(score_surface, score_position)
+
+    @staticmethod
+    def draw_start_prompt(screen, game_resource):
+        start_tip_position = (GAME_AREA_LR + 2 * CELL_WIDTH, GAME_AREA_TOP + 10 * CELL_WIDTH)
+        screen.blit(game_resource.load_newgame_img(), start_tip_position)
